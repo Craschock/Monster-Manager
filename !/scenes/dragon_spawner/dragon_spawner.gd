c7b7bdddb71df1e79da318587ae9f900902b296a
Antonin Masopust <purple.dragon@email.cz>
2026-05-28 18:11:53 +0200

    create dragon spawner

---
+++ !/scenes/dragon_spawner/dragon_spawner.gd
@@ -0,0 +1,51 @@
+extends Node
+
+const MIN_WAIT_TIME = 3
+const MAX_WAIT_TIME = 6
+const DragonScn: PackedScene = preload("res://!/scenes/entities/dragon/dragon.tscn")
+
+@onready var new_dragon_timer: Timer = $NewDragonTimer
+var free_spawn_points: Array[SpawnPoint]
+var occupied_spawn_points: Dictionary[Dragon, SpawnPoint]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for child in get_children():
+		if child is SpawnPoint:
+			free_spawn_points.append(child)
+
+	start_timer()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func start_timer() -> void:
+	new_dragon_timer.start(randi_range(MIN_WAIT_TIME, MAX_WAIT_TIME))
+
+
+func _on_new_dragon_timer_timeout() -> void:
+	print("Spawning..")
+	var spawn_point: SpawnPoint = free_spawn_points.pick_random()
+	if spawn_point:
+		print("There is free space")
+		var dragon: Dragon = DragonScn.instantiate()
+		dragon.position = spawn_point.position
+		dragon.dragon_leaving.connect(_on_dragon_leaving)
+		add_child(dragon)
+		
+		free_spawn_points.erase(spawn_point)
+		occupied_spawn_points[dragon] = spawn_point
+	else:
+		print("All spaces occupied")
+		
+	start_timer()
+
+
+func _on_dragon_leaving(dragon: Dragon) -> void:
+	print("Freeing occupied space")
+	var spawn_point = occupied_spawn_points[dragon]
+	occupied_spawn_points.erase(dragon)
+	free_spawn_points.append(spawn_point)
--- !/scenes/entities/dragon/dragon.gd
+++ !/scenes/entities/dragon/dragon.gd
@@ -3,6 +3,7 @@ extends Node3D
 class_name Dragon
 
 signal dragon_clicked(dragon: Dragon)
+signal dragon_leaving(dragon: Dragon)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,3 +20,9 @@ func _on_model_input_event(_camera: Node, event: InputEvent, _event_position: Ve
 		if event.pressed:
 			print("Dragon clicked")
 			dragon_clicked.emit(self)
+			#leave()
+
+
+func leave() -> void:
+	dragon_leaving.emit(self)
+	queue_free()
