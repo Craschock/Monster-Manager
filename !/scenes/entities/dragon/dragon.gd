05072ab01bd87277af008a873f6661bf035db108
Antonin Masopust <purple.dragon@email.cz>
2026-05-28 16:47:57 +0200

    make dragon clickable

---
+++ !/scenes/entities/dragon/dragon.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+class_name Dragon
+
+signal dragon_clicked(dragon: Dragon)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_model_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+			print("Dragon clicked")
+			dragon_clicked.emit(self)
